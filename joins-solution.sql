ec40ecf0018a89f03aaee46fe012e0a01fbc96f5
Casey Hyde <hyde.casey@gmail.com>
2016-11-17 09:53:33 -0600

    finished

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,12 +1,12 @@
 -- Tasks
--- Get all customers and their addresses.
--- Get all orders and their line items.
--- Which warehouses have cheetos?
--- Which warehouses have diet pepsi?
--- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
--- How many customers do we have?
--- How many products do we carry?
--- What is the total available on-hand quantity of diet pepsi?
+-- 1. Get all customers and their addresses.
+-- 2. Get all orders and their line items.
+-- 3. Which warehouses have cheetos?
+-- 4. Which warehouses have diet pepsi?
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+-- 6. How many customers do we have?
+-- 7. How many products do we carry?
+-- 8. What is the total available on-hand quantity of diet pepsi?
 
 --1.
 SELECT *
@@ -38,9 +38,25 @@ WHERE products.description='diet pepsi' AND warehouse_product.on_hand > 0;
 --Alpha, Delta, and Gamma
 
 --5.
-SELECT first_name, COUNT(*)
+SELECT first_name, last_name, COUNT(*)
 --select customers.first_name, orders.id, addresses.street
 FROM customers
 JOIN addresses ON addresses.customer_id=customers.id
 JOIN orders ON orders.address_id=addresses.id
-GROUP BY first_name;
+GROUP BY first_name, last_name;
+
+--6.
+SELECT COUNT(*)
+FROM customers;
+--4
+
+--7.
+SELECT COUNT(*)
+FROM products;
+--7
+
+--8.
+SELECT SUM(on_hand)
+FROM products
+JOIN warehouse_product ON warehouse_product.product_id=products.id
+WHERE description='diet pepsi';
